7686392a2466f9b9af647c174a02832363d096fb
Parimal Mehta <prmehta24@gmail.com>
2025-06-10 18:15:43 -0500

    Building frontend Chat UI and updating README with progress updates.

--- README.md
+++ README.md
@@ -9,4 +9,10 @@
         * ReactJS + TS for Frontend Chat UI
         * Python for the backend. 
         * Use FastAPI to connect to an OpenAI backend for the AI assistant.
-        * Use Playwright for browser automation.    
+        * Use Playwright for browser automation.  
+* 1 hour complete:
+    * Updated App.tsx to start building the Chat UI. Updated README with link to the guide I am following to build a simple Chat UI.
+
+## References
+* https://medium.com/free-code-camp/how-to-build-a-react-js-chat-app-in-10-minutes-c9233794642b
+
--- frontend/my-app/src/App.tsx
+++ frontend/my-app/src/App.tsx
@@ -1,26 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [messages, setMessages] = useState([
+    {
+      senderId:'OpenAI',
+      text: "Hi, I'm ChatGPT"
+    },
+  ]);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Title/>
+        <MessageList messages = {messages}/>
+        <SendMessageForm/>
       </header>
     </div>
   );
 }
+
+function MessageList({messages} : {messages:any}) {
+  return(
+    <ul className="message-list">                 
+        {messages.map((message: any) => {
+          return (
+           <li key={message.id}>
+             <div>
+               {message.senderId}
+             </div>
+             <div>
+               {message.text}
+             </div>
+           </li>
+         )
+       })}
+     </ul>
+  );
+}
+
+function Title() {
+  return(
+    <div>Title</div>
+  );
+}
+
+function SendMessageForm() {
+  return(
+    <div>SendMessageForm</div>
+  );
+}
+
 
 export default App;
